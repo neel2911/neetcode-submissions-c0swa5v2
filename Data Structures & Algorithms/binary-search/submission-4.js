cee9c4b714f4c697bd70420b91af01afcf8b5cae
Neel Patel <neelrokr@gmail.com>
2026-04-08 20:13:08 -0400

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+        while(l <= r) {
+            let m = Math.floor((l + r) / 2);
+            if(target < nums[m]) {
+                r = m - 1;
+            } else if(target > nums[m]) {
+                l = m + 1;
+            } else {
+                return m
+            }
+        }
+
+        return -1;
+
+    }
+}
