54e3de6e579b620fa5b82311cfadfff624aa4c2c
Neel Patel <neelrokr@gmail.com>
2026-04-12 10:45:20 -0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let start = 1;
+        let end = 1;
+        
+        // Find max
+        for(let pile of piles) {
+            if(end < pile) {
+                end = pile;
+            }
+        }
+
+        let result = end;
+
+        while(start <= end) {
+            const midK = start + Math.floor((end - start) / 2);
+            let totalTime = 0;
+            for(let i = 0; i < piles.length; i++) {
+                const time = Math.ceil(piles[i] / midK);
+                totalTime += time;
+            }
+
+            if(totalTime <= h) {
+                result = midK;
+                end = midK - 1;
+            } else {
+                start = midK + 1;
+            }
+        }
+
+        return result;
+    }
+}
